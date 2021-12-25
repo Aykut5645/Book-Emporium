714f769ce4afc4756a8ebc451bba55e8d057e4ed
Aykut Fatin <aykut.fatin@abv.bg>
2021-12-25 19:25:11 +0200

    Connect my-app to firebase-app

---
+++ src/firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDsjXKGZ1q93KkNzDH6DgHfL8yi5OtuLyM",
+  authDomain: "bookemporium-project.firebaseapp.com",
+  projectId: "bookemporium-project",
+  storageBucket: "bookemporium-project.appspot.com",
+  messagingSenderId: "728085645064",
+  appId: "1:728085645064:web:9a5e66b4f2dbc591940ebd"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
