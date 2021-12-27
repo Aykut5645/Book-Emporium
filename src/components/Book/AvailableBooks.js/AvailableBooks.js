06b933953edf6c284cefef9242b0a3d472e20bc2
Aykut Fatin <aykut.fatin@abv.bg>
2021-12-27 04:05:51 +0200

    Added logic related to fetch all the data from firestore

--- src/components/Book/AvailableBooks.js/AvailableBooks.js
+++ src/components/Book/AvailableBooks.js/AvailableBooks.js
@@ -1,32 +1,30 @@
+import { useState, useEffect } from 'react';
+
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../../firebase-config';
+
 import BookItem from '../BookItem/BookItem';
 
 import classes from './AvailableBooks.module.css';
 
-const booksDb = [
-    {
-        id: 'b1',
-        title: 'It',
-        author: 'Stephen King',
-        price: 20,
-        imageUrl: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1334416842l/830502.jpg'
-    },
-    {
-        id: 'b2',
-        title: 'The Jungle Book',
-        author: 'Rudyard Kipling',
-        price: 19,
-        imageUrl: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1327873594l/77270.jpg'
-
-    },
-    
-];
-
 const AvailableBooks = () => {
-    const bookList = booksDb.map(book => <BookItem key={book.id} book={book} />);
+    const [books, setBooks] = useState([]);
+    const booksCollectionRef = collection(db, 'books');
+
+    useEffect(() => {
+        (async () => {
+            const data = await getDocs(booksCollectionRef);
+            setBooks(
+                data.docs.map(doc => ({ ...doc.data(), id: doc.id }))
+            );
+        })();
+    }, []);
 
     return (
         <ul className={classes.books}>
-            {bookList}
+            {books.map(book =>
+                <BookItem key={book.id} book={book} />
+            )}
         </ul>
     );
 };
