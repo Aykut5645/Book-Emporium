4ded7443c6b8d3d07dd3e752902aaf20a3aefb00
Aykut Fatin <aykut.fatin@abv.bg>
2022-02-03 22:45:51 +0200

    Added logic for store image in firebase

--- src/components/Auth/AuthProfil/AuthProfil.js
+++ src/components/Auth/AuthProfil/AuthProfil.js
@@ -1,23 +1,42 @@
+import { useState, useEffect } from 'react';
+
+import { auth, upload } from '../../../firebase-config';
+
 import Card from '../../UI/Card/Card';
-
 import Button from '../../UI/Button/Button';
-
 import classes from './AuthProfil.module.css';
 import image from '../../../assets/avatar.jpg';
 
 const AuthProfil = () => {
-    const choseFileHandler = () => {
+    const [photo, setPhoto] = useState(null);
+    const [photoUrl, setPhotoUrl] = useState(image);
+
+    useEffect(() => {
+        console.log('outside');
+        if (auth.currentUser?.photoURL) {
+            console.log('inside');
+            setPhotoUrl(auth.currentUser.photoURL);
+        }
+    }, []);
+
+    const changeInputFileHandler = event => {
+        if (event.target.files[0]) {
+            setPhoto(event.target.files[0]);
+        }
+    };
+
+    const uploadHandler = () => {
+        upload(photo, auth.currentUser);
     };
 
     return (
         <Card className={classes.container}>
             <div className={classes["user-img-wrapper"]}>
-                <img src={image} alt="" />
-            </div>
-            <div className={classes['file-wrapper']}>
-                <input type="file" hidden />
-                <label className={classes['file-label']}>Choose File</label>
-                <Button className={classes["user-btn"]}>Upload</Button>
+                <img src={photoUrl} alt="" />
+                <div className={classes['file-wrapper']}>
+                    <input type="file" onChange={changeInputFileHandler} />
+                    <Button onClick={uploadHandler} className={classes["user-btn"]}>Upload</Button>
+                </div>
             </div>
             <div className={classes["user-info"]}>
                 <h2>Change your credentials</h2>
