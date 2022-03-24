137788440ee0fdd9697ee82e9d4244d8feddaaa3
Aykut Fatin <aykut.fatin@abv.bg>
2022-03-24 14:18:49 +0200

    Added link button into initial page to catalog page

--- src/components/Book/BookDetails/BookDetails.js
+++ src/components/Book/BookDetails/BookDetails.js
@@ -59,38 +59,4 @@ const BookDetails = () => {
     );
 };
 
-/*
-<Card className={classes.container}>
-            <div className={classes['book-content']}>
-                <h2>About Book</h2>
-                <div className={classes['content-info']}>
-                    <p><span>Title: </span>{book?.title}</p>
-                    <p><span>Author: </span>{book?.author}</p>
-                    <p><span>Genre: </span>{book?.genre}</p>
-                    <p><span>Price: </span>{book?.price}</p>
-                    <p><span>Contacts: </span>{book?.contacts}</p>
-                </div>
-                <Button>Add to Cart</Button>
-                <Button>Delete</Button>
-                <Link to={`/books/${book?.id}/edit`}>
-                    <Button>Edit</Button>
-                </Link>
-            </div>
-            <div className={classes['book-img-wrapper']}>
-                <img src={book?.imageUrl} alt="" />
-            </div>
-            <div className={classes['book-content-edition']}>
-                <h2>About Edition</h2>
-                <div className={classes['content-info']}>
-                    <p><span>Edition: </span>{book?.edition}</p>
-                    <p><span>Condition: </span>{book?.condition}</p>
-                    <p><span>Year: </span>{book?.year}</p>
-                    <p><span>Pages: </span>{book?.pages}</p>
-                    <p><span>Covers: </span>{book?.covers}</p>
-                </div>
-                <Button className={'last-btn'}>Add to Cart</Button>
-            </div>
-        </Card>
-*/
-
 export default BookDetails;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,17 @@
-import BookSummary from "../components/Book/BookSummary/BookSummary";
 import { Link } from 'react-router-dom';
+
+import BookSummary from "../components/Book/BookSummary/BookSummary";
+import Button from '../components/UI/Button/Button';
 
 const Home = () => {
     return (
         <div className={'initial-image'}>
             <BookSummary />
-            <Link to="/profil">PROFILE</Link>
+            <Link to="/books">
+                <Button className="initial-btn">See Available Books</Button>
+            </Link>
         </div>
     );
 };
 
 export default Home;
-
-/*
-<div
-    style={{
-        backgroundImage: `url(${booksImage})`,
-        height: '100vh',
-        width: '100%',
-        display: 'inline-block'
-    }}>
-    <BookSummary />
-</div>
-*/
