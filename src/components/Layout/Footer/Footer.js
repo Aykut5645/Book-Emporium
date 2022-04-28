a78d8546b8a7a4e3d1020afc64a950f425b26159
Aykut Fatin <aykut.fatin@abv.bg>
2022-04-28 17:44:44 +0300

    Added links into footer components related to facebook, twitter and instagram

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,17 +1,19 @@
+import { Link } from 'react-router-dom';
+
 import classes from './Footer.module.css';
 
 const Footer = () => {
     return (
         <section className={classes.footer}>
             <article className={classes['footer-info']}>
-                <p>Contacts</p>
-                <p>Terms and Conditions</p>
-                <p>About Us</p>
+                <Link to="/conctacts">Contacts</Link>
+                <Link to="/terms">Terms and Conditions</Link>
+                <Link to="/about">About Us</Link>
             </article>
             <article className={classes['footer-icons']}>
-                <span><i className="fab fa-facebook-f"></i></span>
-                <span><i className="fab fa-instagram"></i></span>
-                <span><i className="fab fa-twitter"></i></span>
+                <a href="http://facebook.com"><i className="fab fa-facebook-f"></i></a>
+                <a href="http://instagram.com"><i className="fab fa-instagram"></i></a>
+                <a href="http://twitter.com"><i className="fab fa-twitter"></i></a>
             </article>
             <p>© This site is made with educational purpose only! No rights reserved!</p>
         </section>
