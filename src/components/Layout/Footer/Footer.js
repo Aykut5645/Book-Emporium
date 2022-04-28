e0751d0f3188d9f7fd9ae69d713a1a56fdaf7ead
Aykut Fatin <aykut.fatin@abv.bg>
2022-04-28 14:39:04 +0300

    Refactor Footer

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -3,11 +3,17 @@ import classes from './Footer.module.css';
 const Footer = () => {
     return (
         <section className={classes.footer}>
-            <article className={classes['footer_col']}>
-                <h3 className={classes['footer_col_title']}>SDFASDFSADFSAD</h3>
-                <span><i className={['fab fa-facebook-f']}></i></span>
-                <span><i className={["fab fa-instagram"]}></i></span>
+            <article className={classes['footer-info']}>
+                <p>Contacts</p>
+                <p>Terms and Conditions</p>
+                <p>About Us</p>
             </article>
+            <article className={classes['footer-icons']}>
+                <span><i className="fab fa-facebook-f"></i></span>
+                <span><i className="fab fa-instagram"></i></span>
+                <span><i className="fab fa-twitter"></i></span>
+            </article>
+            <p>© This site is made with educational purpose only! No rights reserved!</p>
         </section>
     );
 };
